be86f0de7dac242dc5d2d2ce6c0905f42f7866ed
Madge P <mepresnell@gmail.com>
2026-01-17 16:06:18 -0500

    add wheel of excitement hosts

--- README.md
+++ README.md
@@ -1,30 +1,30 @@
 # Neopets Stuff by Egg <img src="https://images.neopets.com/community/hub/calendar/events/ixi.png">
-last update: Jan 15 2026
+last update: Jan 17 2026
 
 # Books Read Search and Sort
-want to quickly check if you've read that book? just search it up with this upgrade to the 'Books Read' page.<br><br>
-<img width="500px" height="auto" src="https://i.imgur.com/dvqob15.png">
+want to quickly check if you've read that book? just search it up with this upgrade to the 'Books Read' page.<br>
 * adds a search bar to look for titles in the book list.
 * adds a button to sort the book list alphabetically by title.
 * restyles and resizes stuff for a better experience in general.
-* adds a link back to the regular books page from the booktastic books page.
+* adds a link back to the regular books page from the booktastic books page.<br>
+<img width="600px" height="auto" src="https://i.imgur.com/dvqob15.png">
 
 # Stamp Album Page Flipper
-for browsing your stamp collection (or someone else's) with ease.<br><br>
-<img width="500px" height="auto" src="https://i.imgur.com/w7oRkfg.png">
+for browsing your stamp collection (or someone else's) with ease.<br>
 * adds arrow buttons that will bring you to the next/previous stamp page.
 * organizes stamp page links for easier reading, and highlights the page you're currently on.
 * compatible with popular stamp album helper scripts.<br>
+<img width="600px" height="auto" src="https://i.imgur.com/w7oRkfg.png">
+
 
 # Quest Log Streak View
-forgot to claim your prize because you didn't click on "Streak Reward" and notice it was ready? not anymore!<br><br>
-<img width="600px" height="auto" src="https://i.imgur.com/WtcRNXK.png">
+forgot to claim your prize because you didn't click on "Streak Reward" and notice it was ready? not anymore!<br>
 * shows your daily quest progress and streak progress at the same time, similar to how it looked in the original Quest Log.
 * hides the extra "Let's Go" section under your quests.<br>
+<img width="600px" height="auto" src="https://i.imgur.com/WtcRNXK.png">
 
 # Gormball MVP
-a Gormball companion script for lovers of the game.<br><br>
-<img width="400px" height="auto" src="https://i.imgur.com/4pe2a4j.png"><br>
+a Gormball companion script for lovers of the game.<br>
 * displays your possible score - aka the neopoints you will earn - if you win the game.
 * shows an image of the player in action when it's their move.
 * UPDATE: turns score red when you're in the "danger zone" (can only get negative bonuses)
@@ -32,6 +32,14 @@ a Gormball companion script for lovers of the game.<br><br>
 * adds a link to the Gormball Neopedia page to read up on past championship games.
 * if you're not using the Ruffle extension, replaces the broken flash animation with a piccy.
 * replaces the player bio images with images of their plushies, because that's pretty cute.
+<img width="400px" height="auto" src="https://i.imgur.com/4pe2a4j.png">
+
+# Wheel of Excitement Faerie Hosts
+have a familiar faerie face host the Wheel of Excitement, just for fun.<br>
+* <b><ins>instructions:</ins></b> edit the script and change <i>myFaerie</i> to the faerie of your choice! she'll be there at the wheel and react to your spins.
+* options: Jhudora, Illusen, Fyora, Luxinia, Baelia, Naia, Seshatia (default is Jhudora)
+* how would some of them even be there? i dunno, don't worry about it.<br>
+<img width="500px" height="auto" src="https://i.imgur.com/xDtjuuQ.png">
 
 # Better Community Central
 makes the Community Central page nicer to use.
---
+++ wheelOfEHosts.user.js
@@ -0,0 +1,97 @@
+// ==UserScript==
+// @name         Wheel of Excitement Faerie Hosts
+// @version      2026-01-13
+// @description  WoE is hosted by Jhudora or another faerie of your choosing: Illusen, Fyora, Luxinia, Baelia, Naia, or Seshatia
+// @author       grumpyegg
+// @match        https://www.neopets.com/faerieland/wheel.phtml
+// @icon         https://images.neopets.com/community/hub/calendar/events/ixi.png
+// @downloadURL  https://github.com/grumpyegg/neopets-stuff/raw/refs/heads/main/wheelOfEHosts.user.js
+// @updateURL    https://github.com/grumpyegg/neopets-stuff/raw/refs/heads/main/wheelOfEHosts.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+// choose your faerie host! enter their exact name in the quotes:
+// Jhudora
+// Illusen
+// Fyora
+// Luxinia
+// Baelia
+// Naia
+// Seshatia
+const myFaerie = 'Jhudora';
+
+const IMAGES = { // thank you drsloth.com
+    jhudoraDefault: 'https://images.neopets.com/faeriefestival/2024/np/characters/JhudoraHappyLeft.png',
+    jhudoraGood: 'https://images.neopets.com/faeriefestival/2024/np/characters/JhudoraSmirkingLeft.png',
+    jhudoraBad: 'https://images.neopets.com/faeriefestival/2024/np/characters/JhudoraAngryLeft.png',
+    illusenDefault: 'https://images.neopets.com/faeriefestival/2024/np/characters/IllusenHappyLeft.png',
+    illusenGood: 'https://images.neopets.com/faeriefestival/2024/np/characters/IllusenHappySingLeft_i9k2akmk7i.png',
+    illusenBad: 'https://images.neopets.com/faeriefestival/2024/np/characters/IllusenAngryLeft.png',
+    fyoraDefault: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/fyora_serious_left_67c2ennhc5.png',
+    fyoraGood: 'https://images.neopets.com/faeriefestival/2024/np/characters/FyoraHappyLeft.png',
+    fyoraBad: 'https://images.neopets.com/winter/advent/2023/dialogue_r0c67gl903/actors/FyoraWorriedRight.png',
+    luxiniaDefault: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/luxinia_default_left_d478bf71ig.png',
+    luxiniaGood: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/luxinia_relieved_left_2cgc989m9f.png',
+    luxiniaBad: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/luxinia_upset_left_c7llg0j1g3.png',
+    baeliaDefault: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/baelia_slightsmile_left_3maemk4d65.png',
+    baeliaGood: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/baelia_confused_left_9ac442caeb.png',
+    baeliaBad: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/baelia_embarrassed_left_e3ccc1db2c.png',
+    naiaDefault: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/naia_happy_left_af6246l2jl.png',
+    naiaGood: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/naia_happy_left_af6246l2jl.png',
+    naiaBad: 'https://images.neopets.com/plots/tvw/story/dialogue/actors/naia_worried_left_d4e8mjei21.png',
+    seshatiaDefault: 'https://images.neopets.com/faeriefestival/2024/np/characters/SeshatiaHappyLeft_hggifb5c50.png',
+    seshatiaGood: 'https://images.neopets.com/faeriefestival/2024/np/characters/SeshatiaHappyLeft_hggifb5c50.png',
+    seshatiaBad: 'https://images.neopets.com/faeriefestival/2024/np/characters/SeshatiaWorriedLeft_e6f4ijjf1a.png'
+}
+
+switch (myFaerie.toLowerCase()) {
+    case 'jhudora':
+        GM_addStyle(`
+          #wheelCharacter.wheelChar { background-image: url("${IMAGES.jhudoraDefault}"); }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.jhudoraGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.jhudoraBad}"); }
+        `);
+        break;
+    case 'illusen':
+        GM_addStyle(`
+          #wheelCharacter { background-image: url("${IMAGES.illusenDefault}"); }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.illusenGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.illusenBad}"); }
+        `);
+        break;
+    case 'fyora':
+        GM_addStyle(`
+          #wheelCharacter { background-image: url("${IMAGES.fyoraDefault}"); }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.fyoraGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.fyoraBad}"); }
+        `);
+        break;
+    case 'luxinia':
+        GM_addStyle(`
+          #wheelCharacter { background-image: url("${IMAGES.luxiniaDefault}") ; }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.luxiniaGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.luxiniaBad}"); }
+        `);
+        break;
+    case 'baelia':
+        GM_addStyle(`
+          #wheelCharacter { background-image: url("${IMAGES.baeliaDefault}") ; }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.baeliaGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.baeliaBad}"); }
+        `);
+        break;
+    case 'naia':
+        GM_addStyle(`
+          #wheelCharacter { background-image: url("${IMAGES.naiaDefault}") ; }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.naiaGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.naiaBad}"); }
+        `);
+        break;
+    case 'seshatia':
+        GM_addStyle(`
+          #wheelCharacter { background-image: url("${IMAGES.seshatiaDefault}") ; }
+          #wheelCharacter.wheelChar.wheelChar-good { background-image: url("${IMAGES.seshatiaGood}"); }
+          #wheelCharacter.wheelChar.wheelChar-bad { background-image: url("${IMAGES.seshatiaBad}"); }
+        `);
+        break;
+}
